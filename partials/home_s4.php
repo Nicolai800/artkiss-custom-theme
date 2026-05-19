95f4f5bebf6d695da828e73f306d144bc7f97168
Nicolai800 <happy455420@gmail.com>
2026-05-19 21:12:34 +0200

    home-s4, about as: DONE

--- partials/home_s4.php
+++ partials/home_s4.php
@@ -5,16 +5,32 @@ $section = get_field('about_us_section');
 if (!$section) {
     return;
 }
-$title = $section['title'] ?? '';
+
+$title       = $section['title']       ?? '';
 $description = $section['description'] ?? '';
-$img = $section['img'] ?? false;
+$img         = $section['img']         ?? false;
 ?>
 
 <section class="home-s4">
     <div class="home-s4__container l-container">
-        <div class="home-s4__header">
-            <?php if ($title) : ?>
-                <h2 class="home-s4__title heading2"><?php echo esc_html($title); ?></h2>
+
+        <?php if ($title) : ?>
+            <h2 class="home-s4__title heading2"><?php echo esc_html($title); ?></h2>
+        <?php endif; ?>
+
+        <div class="home-s4__card">
+
+            <?php if ($img) : ?>
+                <div class="home-s4__img-wrap">
+                    <img
+                        class="home-s4__img"
+                        src="<?php echo esc_url($img['url']); ?>"
+                        alt="<?php echo esc_attr($img['alt'] ?? $title); ?>"
+                        width="<?php echo esc_attr($img['width'] ?? ''); ?>"
+                        height="<?php echo esc_attr($img['height'] ?? ''); ?>"
+                        loading="lazy"
+                    >
+                </div>
             <?php endif; ?>
 
             <?php if ($description) : ?>
@@ -22,6 +38,8 @@ $img = $section['img'] ?? false;
                     <?php echo wp_kses_post($description); ?>
                 </div>
             <?php endif; ?>
+
         </div>
+
     </div>
 </section>
