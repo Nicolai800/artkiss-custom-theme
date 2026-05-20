36fa753646d252ba645fa62a0a50ed0e6796833a
Nicolai800 <happy455420@gmail.com>
2026-05-20 21:29:37 +0200

    feat: add trust cards block and style WooCommerce single product page

---
+++ partials/home_s5.php
@@ -0,0 +1,54 @@
+<?php
+$section = get_field('trust_section');
+
+if (!$section) {
+    return;
+}
+
+$title = $section['title'] ?? '';
+?>
+
+<section class="home-s5">
+    <div class="home-s5__container l-container">
+
+        <?php if ($title) : ?>
+            <h2 class="home-s5__title heading2"><?php echo esc_html($title); ?></h2>
+        <?php endif; ?>
+
+        <div class="home-s5__grid">
+            <?php for ($i = 1; $i <= 4; $i++) : 
+                $icon  = $section["card_{$i}_icon"] ?? null;
+                $card_title = $section["card_{$i}_title"] ?? '';
+                $card_text  = $section["card_{$i}_text"] ?? '';
+                
+                if (!$card_title && !$card_text) continue;
+            ?>
+                <div class="home-s5__card">
+                    <?php if ($icon) : ?>
+                        <div class="home-s5__card-icon-wrap">
+                            <img 
+                                class="home-s5__card-icon"
+                                src="<?php echo esc_url($icon['url']); ?>" 
+                                alt="<?php echo esc_attr($icon['alt'] ?? $card_title); ?>" 
+                                width="<?php echo esc_attr($icon['width'] ?? ''); ?>"
+                                height="<?php echo esc_attr($icon['height'] ?? ''); ?>"
+                                loading="lazy"
+                            >
+                        </div>
+                    <?php endif; ?>
+                    
+                    <div class="home-s5__card-content">
+                        <?php if ($card_title) : ?>
+                            <h3 class="home-s5__card-title"><?php echo esc_html($card_title); ?></h3>
+                        <?php endif; ?>
+                        
+                        <?php if ($card_text) : ?>
+                            <p class="home-s5__card-text"><?php echo esc_html($card_text); ?></p>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            <?php endfor; ?>
+        </div>
+
+    </div>
+</section>
--- template-homepage.php
+++ template-homepage.php
@@ -11,5 +11,6 @@ get_template_part('partials/home_s1');
 get_template_part('partials/home_s2');
 get_template_part('partials/home_s3');
 get_template_part('partials/home_s4');
+get_template_part('partials/home_s5');
 
 get_footer();
